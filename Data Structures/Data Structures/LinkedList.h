e2954c75aa14ce3c844c57eb00605d4aa63659dc
Jackson Peven <jdpeven@gmail.com>
2016-10-16 18:13:24 -0700

    Added LL_print
    Linked List now has a print function

--- Data Structures/Data Structures/LinkedList.h
+++ Data Structures/Data Structures/LinkedList.h
@@ -1,1 +1,26 @@
-#pragma once
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+//Here will be all the code for the linked list operations
+
+struct Node
+{
+	int data;
+	struct Node *next;
+}
+
+void Print(Node *head)
+{
+	while (head != nullptr)
+	{
+		cout << head->data << endl;
+		head = head->next;
+	}
+}
+
+
+
+
+
+#endif // !LINKEDLIST_H
+
--- Data Structures/Data Structures/main.cpp
+++ Data Structures/Data Structures/main.cpp
@@ -1,4 +1,15 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <chrono>
 
+#include "LinkedList.h"
 
 int main(void)
 {
