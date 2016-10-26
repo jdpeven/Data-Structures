0179a9655e701f8c9a5896e296a21a5d816f94a0
Jackson Peven <jdpeven@gmail.com>
2016-10-26 12:30:46 -0700

    I made a mistake
    I made a mistake in making my branches in the same file, so trying to
    merge them together was throwing a bunch of errors. Thankfully I caught
    it early so I was able to just copy the data I needed from the branches
    and manually change master. I will now delete the branches once I know
    things are saved in master

--- Data Structures/Data Structures/LinkedList.h
+++ Data Structures/Data Structures/LinkedList.h
@@ -20,40 +20,214 @@ struct Node
 {
 	int data;
 	struct Node *next;
+
+	bool operator < (const Node& Node2)
+	{
+		if (this->data < Node2.data)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	bool operator <= (const Node& Node2)
+	{
+		if (this->data <= Node2.data)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	bool operator > (const Node& Node2)
+	{
+		if (this->data > Node2.data)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	bool operator >= (const Node& Node2)
+	{
+		if (this->data >= Node2.data)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	bool operator == (const Node& Node2)
+	{
+		if (this->data == Node2.data)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 };
 
 class LinkedList
 {
 private:
-	Node * head;
+	Node * _head;
 public:
+	LinkedList()
+	{
+		Node* head = new Node;
+		head = nullptr;
+		_head = head;
+	}
+
 	LinkedList(int data)
 	{
 		Node * head = new Node;
 		head->data = data;
 		head->next = nullptr;
+		_head = head;
 	}
+
 	LinkedList(Node * node)
 	{
-		Node * head = new Node;
-		head = node;
+		//Node * head = new Node;
+		_head = node;
 	}
+
+	Node * get_head()
+	{
+		return _head;
+	}
+
 	void Print()
 	{
-		while (head != nullptr)
+		Node* n = _head;
+		while (n != nullptr)
 		{
-			cout << head->data << endl;
-			head = head->next;
+			cout << n->data << endl;
+			n = n->next;
 		}
+	}
+
+	void insertVal(int data)
+	{
+		Node * new_node = new Node;
+		new_node->data = data;
+		new_node->next = _head;
+		_head = new_node;
 	}
 
 	void insertValEnd(int data)
 	{
 		Node * new_node = new Node;
-		new_node->data = data;
-		new_node->next = nullptr;
-		Node * iter = head;
+		Node * h = new Node;
+		if (_head == nullptr)
+		{
+			h->next = nullptr;
+			h->data = data;
+			_head = h;
+		}
+		else
+		{
+			h = _head;
+			while (h->next != nullptr)
+			{
+				h = h->next;
+			}
+			new_node->data = data;
+			new_node->next = nullptr;
+			h->next = new_node;
 
+			return;
+		}
+	}
+
+	void insertAtPosition(int data, int index)
+	{
+		Node * new_node = new Node;
+		Node * h = new Node;
+		Node * ind_node = new Node;
+		Node * temp = new Node;
+		h = _head; //This will be the constant pointer to the head of the list
+		ind_node = _head; //This will be the one iterated through
+		if (h == nullptr) //linked list is empty
+		{
+			h->next = nullptr;
+			h->data = data;
+			_head = h;
+		}
+		new_node->next = nullptr;
+		new_node->data = data;
+		if (index == 0) //insert at head
+		{
+			new_node->next = _head;
+			_head = new_node;
+		}
+		else
+		{
+			while (h != nullptr)
+			{
+				if (index == 1) //reached the index
+				{
+					temp = ind_node->next;
+					ind_node->next = new_node;
+					new_node->next = temp;
+					return;
+				}
+				else
+				{
+					ind_node = ind_node->next;
+					index--;
+				}
+			}
+		}
+		return;
+	}
+
+	void Delete(int index)
+	{
+		Node * iter_node = new Node;
+		iter_node = _head;
+		if (index == 0) //delete head
+		{
+			_head = _head->next;
+			return;
+		}
+		else
+		{
+			while (index != 1)
+			{
+				iter_node = iter_node->next;
+				index--;
+			}
+			//Now at the node to delete
+			iter_node->next = iter_node->next->next;
+		}
+	}
+
+	void printInReverse()
+	{
+
+	}
+
+	void insertNodeEnd(Node * new_node)
+	{
+		if (_head == nullptr)
+		{
+			_head = new_node;
+			return;
+		}
+		Node * iter = _head;
 		while (iter->next != nullptr)
 		{
 			iter = iter->next;
@@ -62,20 +236,135 @@ public:
 		return;
 	}
 
-	void insertNodeEnd(Node * new_node)
+	void reverse()
 	{
-		Node * iter = head;
-		while (iter->next != nullptr)
+		Node *h = _head;
+		Node *temp = nullptr;
+		Node *cur = nullptr;
+		while (h != nullptr)
 		{
+			temp = h->next;
+			h->next = cur;
+			cur = h;
+			h = temp;
+		}
+		_head = cur;
+		return;
+	}
+
+	int GetNodeFromHead(int position)
+	{
+		Node * h = new Node;
+		h = _head;
+		while (position != 0)
+		{
+			h = h->next;
+			position--;
+		}
+		return h->data;
+	}
+
+	int GetNodeFromTail(int position)
+	{
+		LinkedList revList(this->get_head());
+		revList.reverse();
+		return revList.GetNodeFromHead(position);
+	}
+
+	void DeleteRepeat()//Given a sorted linked list it will delete the repeated elements
+	{
+		Node * temp = new Node;
+		Node * h = new Node;
+		Node * iter = new Node;
+		h = _head;
+		iter = _head;
+		while (iter != nullptr)
+		{
+			if (iter->data == iter->next->data) //values are the same
+			{
+				temp = iter->next;
+				if (temp->next != nullptr)
+				{
+					while (iter->data == temp->next->data)
+					{
+						temp = temp->next;
+					}
+				}
+				iter->next = temp->next;
+			}
 			iter = iter->next;
 		}
-		iter->next = new_node;
-		return;
+		_head = h;
+	}
+
+	bool operator == (const LinkedList& LL2)
+	{
+		Node * LL1_head = new Node;
+		Node * LL2_head = new Node;
+		LL1_head = this->_head;
+		LL2_head = LL2._head;
+		while (LL1_head == LL2_head)
+		{
+			if (LL1_head->next == nullptr && LL2_head->next == nullptr)
+			{
+				return true;
+			}
+			else
+			{
+				LL1_head = LL1_head->next;
+				LL2_head = LL2_head->next;
+			}
+		}
+		return false;
 	}
 
 
 
 };
+LinkedList MergeSorted(LinkedList LL1, LinkedList LL2)
+{
+	LinkedList new_LL;
+	Node * head1 = new Node;
+	Node * head2 = new Node;
+	head1 = LL1.get_head();
+	head2 = LL2.get_head();
+	Node * temp = new Node;
+	while (head1 != nullptr && head2 != nullptr) //both not empty
+	{
+		if (head1->data <= head2->data)//Used operator overloading in the Node struct
+		{
+			//must use temp otherwise when inserting the node, it would insert all the "next"
+			temp = head1;
+			head1 = head1->next;
+			temp->next = nullptr;
+			new_LL.insertNodeEnd(temp);
+
+		}
+		else if (head2->data < head1->data)
+		{
+			temp = head2;
+			head2 = head2->next;
+			temp->next = nullptr;
+			new_LL.insertNodeEnd(temp);
+		}
+	}
+	if (head1 == nullptr && head2 == nullptr)
+	{
+		return new_LL;
+	}
+	if (head1 == nullptr)//head1 is empty, head2 still has some
+	{
+		new_LL.insertNodeEnd(head2);
+	}
+	else if (head2 == nullptr)//head2 is empty, head1 still has some
+	{
+		new_LL.insertNodeEnd(head1);
+	}
+	return new_LL;
+}
+
+
+
 
 
 
--- Data Structures/Data Structures/main.cpp
+++ Data Structures/Data Structures/main.cpp
@@ -14,23 +14,59 @@
 
 int main(void)
 {
-	BST_NODE myBST(10);
-	/*LinkedList myLL(0);
+	/*
+	//BST_NODE myBST(10);
+	LinkedList myLL1;
+	LinkedList myLL2;
 
-	myLL.insertValEnd(0);
-	myLL.insertValEnd(3);
-	myLL.insertValEnd(5);
-	myLL.insertValEnd(4);
+	myLL1.insertVal(10);
+	myLL1.insertVal(10);
+	myLL1.insertVal(8);
+	myLL1.insertVal(2);
+	myLL1.insertVal(2);
 
+	myLL1.Print();
+
+	cout << "Deleting the repeats" << endl;
+	myLL1.DeleteRepeat();
+	myLL1.Print();
+
+	/*cout << "The first position is " << myLL1.GetNodeFromHead(1) << endl;
+	cout << "The first position from the end is " << myLL1.GetNodeFromTail(1) << endl;*/
+	/*myLL2.insertVal(15);
+	myLL2.insertVal(12);
+	myLL2.insertVal(5);
+	myLL2.insertVal(2);
+	myLL2.insertVal(1);
+
+	cout << "LL2" << endl;
+	myLL2.Print();
+
+	cout << "Merged" << endl;
+	LinkedList Merged;
+	Merged = MergeSorted(myLL1, myLL2);
+	Merged.Print();
+	*/
+	//cout << (myLL1 == myLL2) << endl;
+	//myLL.insertAtPosition(33, 0);
+
+	/*myLL.Print();
+	myLL.Delete(2);
+	cout << "Deletion at element 2" << endl;
+	myLL.Print();*/
+	/*myLL.reverse();
+	cout << "Reversed" << endl;
 	myLL.Print();*/
 
-	myBST.insert(5);
+	system("pause");
+
+	/*myBST.insert(5);
 	myBST.insert(15);
 	myBST.insert(8);
 	myBST.insert(1);
 	myBST.insert(13);
 
-	myBST.preOrder();
+	myBST.preOrder();*/
 
 	return 0;
 }
